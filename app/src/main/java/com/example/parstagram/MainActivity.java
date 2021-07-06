59e98ae2be258ab2b2afc5bc242f4eed75bc0911
Hannah Kim <hannahkm@fb.com>
2021-07-06 17:48:33 -0400

    section 3 completed
    user can log out of their account

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.parstagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton btUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btUser = findViewById(R.id.btUser);
+    }
+
+    public void logout(View v){
+        ParseUser.logOut();
+        Intent i = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(i);
+
+
     }
 }
